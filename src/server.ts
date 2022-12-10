36292fa8595493bf57e078d3ae4d1f5ad695188f
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-12-10 00:54:50 -0500

    initialize project

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cookieParser from 'cookie-parser';
 
 import { errorHandler } from '../error-handler/error-handler';
-import path from 'path';
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -10,16 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-app.set('view engine', 'hbs');
-
-app.get('/', (req: Request, res: Response) => {
-  res.render('index');
-});
-
-app.get('/page2', (req: Request, res: Response) => {
-  res.render('page2');
-});
 
 app.use(errorHandler);
 
